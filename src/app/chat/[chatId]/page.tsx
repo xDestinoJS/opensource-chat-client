e2d810fd61ac3087323a27c101f4ad07af0fe7b9
xdestinoo <xdestinoo@gmail.com>
2025-06-19 19:06:32 +0000

    add agent id?

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -3,7 +3,6 @@
 import { useParams } from "next/navigation";
 import ChatPage from "../../../components/chat/chat-page";
 import { Id } from "../../../../convex/_generated/dataModel";
-import { useSearchParams } from "next/navigation";
 
 export default function Page() {
 	const { chatId } = useParams();
--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import ChatPage from "../../components/chat/chat-page";
-import { Id } from "../../../convex/_generated/dataModel";
 
 export default function Page() {
-	const searchParams = useSearchParams();
-	const agentId = searchParams.get("agentId");
-
-	return <ChatPage agentId={(agentId ?? "") as Id<"agents">} />;
+	return <ChatPage />;
 }
